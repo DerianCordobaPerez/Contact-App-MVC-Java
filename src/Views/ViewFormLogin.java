c68acddd20a129d9c4134e502fcd8ba8de9133cf
DerianCordobaPerez <derianunanleon@gmail.com>
2020-12-02 17:59:47 -0600

    Modificacion sobre el archivo ViewFormLogin, agregando alguna funcionalidad

--- src/Views/ViewFormLogin.java
+++ src/Views/ViewFormLogin.java
@@ -1,8 +1,17 @@
 package Views;
 
-public class ViewFormLogin {
+import javax.swing.*;
 
+public class ViewFormLogin {
+    private JFrame loginWindow;
     public void renderViewFormLogin() {
-        
+        this.loginWindow = new JFrame("Login");
+        this.loginWindow.setSize(400, 400);
+        this.loginWindow.setVisible(true);
+        componentLogin();
+    }
+
+    private void componentLogin() {
+
     }
 }
